2fc02929091ffdc380b87ae81f0e385b63d6feff
thomasy314 <thomasy314@gmail.com>
2024-12-01 07:37:03 -0800

    Adding artist info to GET portfolio

--- app/artists/routes.py
+++ app/artists/routes.py
@@ -10,11 +10,13 @@ bp = Blueprint('artists', __name__)
 @authorize_request()
 def getArtist(artist_id: str):
     artist_info = spotifyAPI.get_artist(artist_id)
+
     response_data = {
-        'id': artist_info['id'],
-        'name': artist_info['name'],
-        'genres': artist_info['genres'],
-        'popularity': artist_info['popularity'],
-        'follower_count': artist_info['followers']['total']
-    }
+            "id": artist_info['id'],
+            "url": artist_info['external_urls']['spotify'],
+            "followers": artist_info['followers']['total'],
+            "name": artist_info['name'],
+            "popularity": artist_info['popularity'],
+            "type": artist_info['type']
+        }
     return response_data, 200
--- app/positions/routes.py
+++ app/positions/routes.py
@@ -17,6 +17,28 @@ bp = Blueprint("positions", __name__)
 def get_positions(account_id):
     positions = Position.query.filter_by(account_id=account_id).all()
 
-    return [p.serialize for p in positions], 200
+    results = []
+
+    for p in positions:
+        artist_info = spotifyAPI.get_artist(p.asset_id)
+
+        artist_result_data = {
+            "id": artist_info['id'],
+            "url": artist_info['external_urls']['spotify'],
+            "followers": artist_info['followers']['total'],
+            "name": artist_info['name'],
+            "popularity": artist_info['popularity'],
+            "type": artist_info['type']
+        }
+
+        new_result = p.serialize
+
+        del new_result['asset_id']
+        new_result['asset_info'] = artist_result_data
+
+        results.append(new_result)
+
+
+    return results, 200
 
 
